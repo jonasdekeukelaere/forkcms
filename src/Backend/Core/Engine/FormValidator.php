011553ca4453a297bf996ce954335600bae0079b
Jonas De Keukelaere <jonas@sumocoders.be>
2014-11-07 12:43:19 +0000

    Rewrites validator to accept multiple rules

--- src/Backend/Core/Engine/FormValidator.php
+++ src/Backend/Core/Engine/FormValidator.php
@@ -13,6 +13,11 @@ class FormValidator
      */
     protected $rules = array();
 
+    protected $correctedRules = array(
+        'required' => 'filled',
+        'url' => 'URL',
+    );
+
     /**
      * @param ModelForm  $form
      * @param array      $rules
@@ -25,11 +30,41 @@ class FormValidator
 
     public function isValid()
     {
-        foreach ($this->rules as $fieldName => $rule) {
+        foreach ($this->rules as $fieldName => $rules) {
+            // get field
+            if (!$this->form->existsField($fieldName)) {
+                throw new Exception('Field ' . $fieldName . ' does not exist');
+            }
             $field = $this->form->getField($fieldName);
-            $method = 'is' . \SpoonFilter::toCamelCase($rule['rule']);
 
-            $field->$method($rule['message']);
+            // get rules
+            if (empty($rules)) {
+                throw new Exception('Rules not set for field ' . $fieldName);
+            }
+
+            // validate
+            foreach ($rules as $rule => $error) {
+                // get error message
+                $errorMessage = Language::err('FormError');
+                if (isset($error)) {
+                    $errorMessage = $error;
+                } else {
+                    $rule = $error;
+                }
+
+                // validate field
+                if (array_key_exists($rule, $this->correctedRules)) {
+                    $rule = $this->correctedRules[$rule];
+                }
+                $method = 'is' . \SpoonFilter::toCamelCase($rule, '-');
+                if (!method_exists($field, $method)) {
+                    throw new Exception('Rule ' . $rule . ' does not exist for field ' . $fieldName);
+                }
+                if (!$field->$method($errorMessage)) {
+                    // stop when field has error
+                    break;
+                }
+            }
         }
 
         return $this->form->isCorrect();
