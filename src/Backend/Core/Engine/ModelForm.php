940209693879da7552f581d11597ae790a71338e
Jonas De Keukelaere <jonas@sumocoders.be>
2014-11-07 11:53:18 +0000

    Removes specific text method

--- src/Backend/Core/Engine/ModelForm.php
+++ src/Backend/Core/Engine/ModelForm.php
@@ -21,13 +21,4 @@ class ModelForm extends Form
 
         $this->record = $record;
     }
-
-    public function addText($name, $value = null, $maxLength = 255, $class = null, $classError = null, $HTML = false)
-    {
-        if ($value === null && isset($this->record[$name])) {
-            $value = $this->record[$name];
-        }
-
-        return parent::addText($name, $value, $maxLength, $class, $classError, $HTML);
-    }
 }
